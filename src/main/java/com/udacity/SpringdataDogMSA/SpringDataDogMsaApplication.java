5aa20eb6a27a723c8e3ec81bbfec064e4c5a2eb1
Anshu <anshu.ranjan2607@mail.com>
2023-02-20 19:00:20 -0600

    Added files for udacity Dog-MSA project

---
+++ src/main/java/com/udacity/SpringdataDogMSA/SpringDataDogMsaApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.SpringdataDogMSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDogMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDogMsaApplication.class, args);
+	}
+
+}
